027c29783e535267f28307fe731c3d003354b69c
shridpant <shrid.pant8@gmail.com>
2020-12-23 21:45:12 +0545

    In-house meme (error) generator

--- src/meme.py
+++ src/meme.py
@@ -1,20 +1,30 @@
 from PIL import Image, ImageDraw, ImageFont
-import os
+import textwrap, os
 
-# INPUT
-im = Image.open('C:/Users/User/Documents/GitHub/stockie/static/readme/screenshot.png')
-top = "Hello\nOkayayayayayaya"
+def meme(para = 'Hello, World!'):
+    # INPUT
+    im = Image.open('static/meme/cat.jpg')
 
-# CONFIGURATION
-width, height = im.size
-arial = ImageFont.truetype("arial.ttf", size=80)
-draw = ImageDraw.Draw(im)
-t_width, t_height = draw.multiline_textsize(top, font=arial)
-print(width, height)
-print(t_width, t_height)
+    # CONFIGURATION
+    image_width, image_height = im.size
+    arial = ImageFont.truetype("arial.ttf", size = 120)
+    draw = ImageDraw.Draw(im)
+    shadowcolor = 'black'
+    fillcolor = 'white'
+    highlight_width = 5
 
-# TOP
-draw.multiline_text(((width-t_width)/2,(0)), top, font=arial, fill="black")
+    # MULTILINE
+    paragraph = textwrap.wrap(para, initial_indent = '(output) ', placeholder = 'etc etc ...', width = 25, max_lines = 3, break_long_words = True)
+    initial_height, line_spacing = (0.01 * image_height), 1
+    for line in paragraph:
+        text_width, text_height = draw.textsize(line, font = arial)
+        x_coordinate, y_coordinate = (image_width - text_width) / 2, initial_height
+        draw.text((x_coordinate - highlight_width, y_coordinate - highlight_width), line, font = arial, fill=shadowcolor)
+        draw.text((x_coordinate + highlight_width, y_coordinate - highlight_width), line, font = arial, fill=shadowcolor)
+        draw.text((x_coordinate - highlight_width, y_coordinate + highlight_width), line, font = arial, fill=shadowcolor)
+        draw.text((x_coordinate + highlight_width, y_coordinate + highlight_width), line, font = arial, fill=shadowcolor)
+        draw.text((x_coordinate, y_coordinate), line, font = arial, fill = fillcolor)
+        initial_height += text_height + line_spacing
 
-
-im.save("hello.png", "PNG")
+    # OUTPUT
+    im.save('static/meme/output.png', "PNG")
